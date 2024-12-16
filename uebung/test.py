06474535b2af37afe0038142f893eccdf98c5b81
WeisongQi <qiweisong@outlook.com>
2024-12-16 13:08:24 +0100

    test

--- uebung/test.py
+++ uebung/test.py
@@ -1,4 +1,25 @@
-date_1 = "2024.12.11"
-date_2 = "2024.12.12"
+# date_1 = "2024.12.11"
+# date_2 = "2024.12.12"
 
-print(f"date_1 > date_2 {date_1 > date_2}")
+# print(f"date_1 > date_2 {date_1 > date_2}")
+
+
+# def fage():
+#    fage_inp = input("age")
+
+#   print(fage_inp)
+
+
+# fage()
+
+
+# Name
+def fname():
+    fname_inp = input("name")
+
+    # print(fname_inp)
+    return f"name{fname_inp}"
+
+
+fname()
+fname()
--- weisong-121224-aufgabe1.py
+++ weisong-121224-aufgabe1.py
@@ -1,6 +1,24 @@
-# Namensabfragen (Vor- und Nachname)
+# Aufgabe 1 Namensabfragen (Vor- und Nachname)
 
-vorname = input ("Bitte geben Sie Ihren Vornamen ein: ")
-nachname = input ("Bitte geben Sie Ihren Nachnamen ein: ")
+vorname = input("Bitte geben Sie Ihren Vornamen ein: ")
+nachname = input("Bitte geben Sie Ihren Nachnamen ein: ")
 
-print (f"Hallo, {vorname} {nachname}.")
+print(f"Hallo, {vorname} {nachname}.")
+
+# Aufgabe 2 Addition von Zahlen
+
+zahle1 = input("Bitte geben Sie eine Zahl ein: ")
+zahle2 = input("Bitte geben Sie noch eine Zahl ein:")
+
+print(f"Die Zwei Zahlen summen ist: {int(zahle1) + int(zahle2)}.")
+
+# Aufgabe 3 Einfache If-Bedingung
+zahl = input("Bitte geben Sie eine Zahl ein: ")
+zahl_int = int(zahl)
+
+if zahl_int < 0:
+    print("Die Zahl ist negativ.")
+elif zahl_int == 0:
+    print("Die Zahl ist Null.")
+else:
+    print("Die Zahl ist positiv.")
