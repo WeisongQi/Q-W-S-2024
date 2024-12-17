022c0887557f51e07f99f45d72faab7f291a1f41
WeisongQi <qiweisong@outlook.com>
2024-12-17 17:13:07 +0100

    fertig

---
+++ aufgabe-171224-aufgabe3.py
@@ -0,0 +1,93 @@
+# Aufgabe 3
+
+
+# Definieren von Funktionen
+def num_input():
+    num = input("gib the number: ")
+
+    if num.isdigit():
+        print(float(num))
+        return float(num)
+    else:
+        print("error")
+        return num_input()
+
+
+# Fläche eines Rechtecks
+def calc_area():
+    w = num_input()
+    l = num_input()
+    print(f"Die Fläche dieses Vierecks beträgt: {w * l}")
+
+
+# miles to KM
+def miles_to_KM():
+    m = num_input()
+    km = m * 1.60934
+    # print(type(km))
+    print(f"{m} miles = {km} Kilometers")
+
+
+# KM to miles
+def KM_to_miles():
+    km = num_input()
+    m = km / 1.60934
+    # print(type(km))
+    print(f"{m} miles = {km} Kilometers")
+
+
+#  fahrenheit to celsius
+def fahrenheit_to_celsius():
+    fahr = num_input()
+    cels = (fahr - 32) * 5 / 9
+    print(f"{cels} Grad Celsius = {fahr} Grad Fahrenheit")
+
+
+# Taschenrechner
+
+
+# Funktionen
+def add(x, y):
+    print(f"Zahl-A + Zahl-B ergibt {x+y}")
+
+
+def subtract(x, y):
+    print(f"Zahl-A - Zahl-B ist {x-y}")
+
+
+def mult(x, y):
+    print(f"Zahl-A * Zahl-B ist {x*y}")
+
+
+def div(x, y):
+    print(f"Zahl-A / Zahl-B ist {x/y}")
+
+
+def taschenrechner():
+
+    f_aufruf = input(
+        "Welche Rechenmethode wählen Sie: 1:Addition, 2:Subtraktion, 3:Multiplikation, 4:Division (A,S,M;D): "
+    )
+
+    x = num_input()
+    y = num_input()
+
+    if f_aufruf in ["A", "a", "1"]:
+        add(x, y)
+    elif f_aufruf in ["S", "s", "2"]:
+        subtract(x, y)
+    elif f_aufruf in ["M", "m", "3"]:
+        mult(x, y)
+    elif f_aufruf in ["D", "d", "4"]:
+        div(x, y)
+    else:
+        print("Sie haben einen Fehler eingegeben.")
+        return taschenrechner()
+
+
+# main
+calc_area()
+miles_to_KM()
+KM_to_miles()
+fahrenheit_to_celsius()
+taschenrechner()
---
+++ aufgabe-171224-aufgabe4.py
@@ -0,0 +1,57 @@
+# Spiel Schere Stein Papier
+import random
+
+wahl_sum = ["Schere", "Stein", "Papier"]
+
+
+def player_input():
+    spiel_ab = input("Spielen ? y/n : ")
+    if spiel_ab.lower() == "y":
+        player_wahl = input("Schere or Stein or Papier : ")
+        if player_wahl in wahl_sum:
+            # print(f"{player_wahl}")
+            return player_wahl
+        else:
+            print("error")
+            return player_input()
+    else:
+        return None
+
+
+""" def com_player():
+    com_wahl = random.choice(wahl_sum)
+    print(com_wahl)
+    return com_wahl """
+
+
+# Main
+
+
+def main():
+    rules = {"Schere": "Papier", "Papier": "Stein", "Stein": "Schere"}
+    player_wins = 0
+    computer_wins = 0
+    gleich = 0
+    while True:
+        player_choice = player_input()
+        if player_choice is None:
+            break
+        com_player = random.choice(wahl_sum)
+        # print(f"Computer wählt : {com_player}")
+
+        # win ?
+        if com_player == player_choice:
+            print("Gleich")
+            gleich += 1
+        elif rules[player_choice] == com_player:
+            print("Player win")
+            player_wins += 1
+        else:
+            print("Computer Win")
+            computer_wins += 1
+        print(
+            f"Aktueller Spielstand-Spieler: {player_wins} |Computer:{computer_wins} |Gleich:{gleich}"
+        )
+
+
+main()
