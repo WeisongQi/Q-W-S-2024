fc9aef28554490c8a67026d727c506cb9e0100d4
WeisongQi <qiweisong@outlook.com>
2024-12-20 09:40:08 +0100

    f

--- aufgabe-181224-aufgabe2.py
+++ aufgabe-181224-aufgabe2.py
@@ -72,7 +72,7 @@ def wochentag_berechnen():
         0: "Montag",
         1: "Dienstag",
         2: "Mittwoch",
-        3: "Donnestag",
+        3: "Donnerstag",
         4: "Freitag",
         5: "Samstag",
         6: "Sonntag",
--- uebung/test.py
+++ uebung/test.py
@@ -44,9 +44,63 @@ length = len(mix_list)
 print(length) """
 
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
 
-sum = add(3, 5)
-print(f"{add(4, add(3,5))}")
+# sum = add(3, 5)
+# print(f"{add(4, add(3,5))}")
+
+# import datetime
+
+# # Creating a date object
+# date_object = datetime.date(2024, 7, 17)
+# print(f"{type(date_object)}", date_object)
+# # Getting the day name
+# day_name = date_object.strftime("%A")
+
+# # Printing the day name
+# print(f"The day name for {date_object} is: {day_name}")
+
+
+import calendar
+
+from datetime import datetime
+
+
+def wochentag_berechnen():
+    wochentag = input("Bitte nenne ein Datum ")
+    day = datetime.datetime.strptime(wochentag, "%d.%m.%Y").date()
+    print(f"Der {wochentag} ist ein {calendar.day_name[day.weekday()]}\n")
+
+
+def tage_bis_datum():
+    while True:
+        try:
+            # user input datum
+            datum_str = input("Bitte geben Sie ein Datum im Format TT.MM.JJJJ ein: ")
+
+            # stringer to datum
+            datum = datetime.strptime(datum_str, "%d.%m.%Y").date()
+
+            # today
+            heute = datetime.today().date()
+            print(f"{type (heute)}", heute)
+
+            # rechnen
+            differenz = (datum - heute).days
+            print(f"{type (differenz)}", differenz)
+
+            # output
+            print(f"Es sind {abs(differenz.days)} Tage von heute bis zum {datum_str}.")
+            break
+        except ValueError:
+            print("Ungültiges Datum. Bitte versuchen Sie es erneut.")
+
+
+# tage_bis_datum()
+
+# wochentag_berechnen()
+
+spieler_wahl = input("Deine Wahl: ").capitalize()
+print(f"{type spieler_wahl}", spieler_wahl)
